98f04ae1bb2be4543fbe6ad3a9f0bc819c19aeb9
Yuya Kakui <mail@kakuiyuya.com>
2013-04-01 09:02:05 +0900

    add rakuten word search

--- Rakuten.pm
+++ Rakuten.pm
@@ -36,4 +36,24 @@ sub keyword_hotel_search {
   return $rakuten_xml->{Body}->{'KeywordHotelSearch:KeywordHotelSearch'}->{hotel};
 } 
 
+sub item_search {
+  my ($self, $param) = @_;
+  my $DEVELOPER_ID = "bfc5bca21a7bac85a197a29ebeab80dd";
+  my $AFFILIATE_ID = "0e2a74f8.b705f347.0e2a74f9.ce1173da";
+  my $API_BASE_URL = "http://api.rakuten.co.jp/rws/3.0/rest";
+  my $uri = URI->new($API_BASE_URL);
+
+  $uri->query_form(
+    developerId => $DEVELOPER_ID,
+    affiliateId => $AFFILIATE_ID,
+    operation   => "ItemSearch",
+    version     => "2010-09-15",
+    keyword     => $param,
+  );
+  my $response = get($uri);
+  my $xml = new XML::Simple;
+  my $rakuten_xml = $xml->XMLin($response);
+  return $rakuten_xml->{Body}->{'itemSearch:ItemSearch'}->{Items}->{Item};
+} 
+
 1;
--- rakuten.pl
+++ rakuten.pl
@@ -3,37 +3,58 @@ use strict;
 use warnings;
 use XML::Simple;
 use LWP::Simple;
-use Encode;
+use HTML::TreeBuilder;
+use Encode qw(encode_utf8); 
+binmode STDOUT,":utf8";
 use URI;
 use Data::Dumper;
 use CGI;
 use Rakuten;
 
 # CGI Configuration
-my $cginame = 'rakuten_travel.cgi';
-my $TITLE   = '楽天トラベルキーワードサーチAPI';
+#my $cginame = 'rakuten_travel.cgi';
+#my $TITLE   = '楽天トラベルキーワードサーチAPI';
 
 # Rakuten Webservice Copy Right
-my $webservice = <<"EOF";
-<!-- Rakuten Web Service Attrubution Snippet FROM HERE -->
-<a href="http://webservice.rakuten.co.jp/" target="_blank">
-<img src="http://webservice.rakuten.co.jp/img/credit/200709/credit_22121.gif" border="0" alt="楽天ウェブサービスセンター" title="楽天ウェブサービスセンター" width="221" height="21"/></a>
-<!-- Rakuten Web Service Attribution Snippet TO HERE -->
-EOF
+#my $webservice = <<"EOF";
+#<!-- Rakuten Web Service Attrubution Snippet FROM HERE -->
+#<a href="http://webservice.rakuten.co.jp/" target="_blank">
+#<img src="http://webservice.rakuten.co.jp/img/credit/200709/credit_22121.gif" border="0" alt="楽天ウェブサービスセンター" title="楽天ウェブサービスセンター" width="221" height="21"/></a>
+#<!-- Rakuten Web Service Attribution Snippet TO HERE -->
+#EOF
 
 # CGI Parameters
-my $q = new CGI;
-my $keyword = $q->param('keyword');
-
+#my $q = new CGI;
+#my $keyword = $q->param('keyword');
+#
 # Rakuten Webservice API
 my $rakuten = new Rakuten();
-my $rakutenhotels = $rakuten->keyword_hotel_search("品川 ホテル");
+# my $rakutenhotels = $rakuten->keyword_hotel_search("品川 ホテル");
 
-foreach my $rakutenhotel (@$rakutenhotels) {
-  print Dumper $rakutenhotel;
+my $file = "/var/www/html/data/realtime.txt";
+my $items;
+
+open(my $fh, "<", $file)
+  or die "Cannot open $file: $!";
+
+unlink "/var/www/html/data/rakuten.txt";
+
+while(my $line = readline $fh) {
+  chomp $line;
+
+  $items = $rakuten->item_search($line);
+  
+  open(FILE, ">> /var/www/html/data/rakuten.txt");
+  
+  foreach my $item (@$items) {
+    print FILE $item->{mediumImageUrl} . "\n";
+  }
+  
+  close(FILE);
 }
 
 # Display Page
+=pod
 print "Content-Type: text/html\n\n";
 print <<"EOF";
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -49,3 +70,4 @@ print <<"EOF";
 EOF
 
 exit(0);
+=cut
